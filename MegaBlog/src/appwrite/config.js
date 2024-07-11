edccf16ea02a59caa68d52a8d01078f91a693bc1
Vinayak Bandhu <vinayak462000@gmail.com>
2024-07-11 21:01:57 +0530

    mega

---
+++ MegaBlog/src/appwrite/config.js
@@ -0,0 +1,134 @@
+import conf from '../conf.js'
+import { Client, ID, Databases, Storage, Query} from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userID}){
+        try{
+            return await this.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userID
+                }
+            )
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    async updatePost(slug,{title, content, featuredImage, status}){
+        try{
+            return await this.databases.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                }
+            )
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]){
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    // file upload service
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    // file delete service
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    // file preview service
+
+    getFilePreview(fileId){
+        return this.bucket.getFilePreview(
+            conf.appwriteBucketId,
+            fileId
+        )
+    }
+}
+
+const service = new Service()
+export default service
